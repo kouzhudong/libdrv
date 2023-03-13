59bc25e39b18e5765ccfe48d7c17f18a13b4d9b0
correy <112426112@qq.com>
2023-03-14 07:47:30 +0800

    改进。

--- test/DriverEntry.cpp
+++ test/DriverEntry.cpp
@@ -45,18 +45,12 @@ EXTERN_C NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_ST
 
     PAGED_CODE();
 
+    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_INFO_LEVEL, 
+               "FILE:%s, LINE:%d, DATE:%s, TIME:%s.\r\n", __FILE__, __LINE__, __DATE__, __TIME__);
+
     DriverObject->DriverUnload = Unload;
 
     //TestUseNoExecuteMemory();
-
-    //HideDriver(DriverObject);
-
-    //GetKernel32FullPath();
-    //GetSomeSystemRoutineAddress();
-
-    //HANDLE UniqueProcess = NULL;
-    //ASSERTMSG("����д������PID", UniqueProcess);
-    //IsWow64Process(UniqueProcess);
 
     //CreateSystemThreadInIdleProcess();
 
