4e365d9b715daf8767dbf20656280159d4323adc
correy <112426112@qq.com>
2025-12-08 11:15:12 +0800

    改进。

--- libdrv/cpp.cpp
+++ libdrv/cpp.cpp
@@ -1,8 +1,30 @@
 #include "cpp.h"
 
 
+#pragma warning(disable : 5043) //异常规范与前面的声明不匹配
+
+
 _IRQL_requires_max_(DISPATCH_LEVEL)
-void * __cdecl operator new(_In_ size_t size)
+void * __cdecl operator new(size_t size, void * p) noexcept
+// ============== 定位 new (Placement New) (必须存在) ==============
+// 定位 new 必须保持原样，它不分配内存，只返回指针
+{
+    UNREFERENCED_PARAMETER(size);
+    return p;
+}
+
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void __cdecl operator delete(void * p, void * q) noexcept
+//定位 delete (当定位 new 的构造函数失败时调用)
+{
+    UNREFERENCED_PARAMETER(p);
+    UNREFERENCED_PARAMETER(q);
+}
+
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void * __cdecl operator new(_In_ size_t size) noexcept
 {
     if (size == 0) {
         return nullptr;
@@ -18,7 +40,7 @@ void * __cdecl operator new(_In_ size_t size)
 
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
-void __cdecl operator delete(_In_ void * p)
+void __cdecl operator delete(_In_ void * p) noexcept
 {
     if (p) {
         ExFreePoolWithTag(p, TAG);
@@ -27,24 +49,26 @@ void __cdecl operator delete(_In_ void * p)
 
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
-void __cdecl operator delete(_In_ void * p, _In_ SIZE_T size)
+void __cdecl operator delete(_In_ void * p, _In_ SIZE_T size) noexcept
 {
     UNREFERENCED_PARAMETER(size);
 
     if (p) {
-        ExFreePoolWithTag(p, TAG);
+        //ExFreePoolWithTag(p, TAG);
+        operator delete(p);
     }
 }
 
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
-void * __cdecl operator new[](_In_ size_t size)
+void * __cdecl operator new[](_In_ size_t size) noexcept
 {
     if (size == 0) {
         return nullptr;
     }
 
-    const auto p = ExAllocatePoolWithTag(NonPagedPool, size, TAG);
+    //const auto p = ExAllocatePoolWithTag(NonPagedPool, size, TAG);
+    const auto p = operator new(size);
     if (p) {
         RtlZeroMemory(p, size);
     }
@@ -54,19 +78,21 @@ void * __cdecl operator new[](_In_ size_t size)
 
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
-void __cdecl operator delete[](_In_ void * p)
+void __cdecl operator delete[](_In_ void * p) noexcept
 {
     if (p) {
-        ExFreePoolWithTag(p, TAG);
+        //ExFreePoolWithTag(p, TAG);
+        operator delete(p);
     }
 }
 
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
-void __cdecl operator delete[](_In_ void * p, _In_ SIZE_T size)
+void __cdecl operator delete[](_In_ void * p, _In_ SIZE_T size) noexcept
 {
     UNREFERENCED_PARAMETER(size);
     if (p) {
-        ExFreePoolWithTag(p, TAG);
+        //ExFreePoolWithTag(p, TAG);
+        operator delete(p);
     }
 }
